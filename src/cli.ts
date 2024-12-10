5f105376cb9f54edf61d0da79f012caf2be240ff
Yudi Kubota <yudikubota@gmail.com>
2024-12-10 17:41:00 -0300

    chore: consolidating header to reduce dependencies / add docs link [AF-281]

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import figlet from 'figlet';
 import { Command } from "commander";
 import { init } from "./commands/init";
 import { authenticate } from "./commands/auth/auth";
@@ -11,16 +10,28 @@ import { logout } from './commands/logout';
 import { fix } from './commands/deploy/fix';
 import { version } from '../package.json';
 
+function printHeader() {
+  const headerText = `     _                 __ _                  ____ _     ___
+    / \\   _ __  _   _ / _| | _____      __  / ___| |   |_ _|
+   / _ \\ | '_ \\| | | | |_| |/ _ \\ \\ /\\ / / | |   | |    | |
+  / ___ \\| | | | |_| |  _| | (_) \\ V  V /  | |___| |___ | |
+ /_/   \\_\\_| |_|\\__, |_| |_|\\___/ \\_/\\_/    \\____|_____|___|
+                |___/
+`;
+  console.log(headerText);
+}
+
 async function main() {
-  console.log("Starting AnyFlow CLI...");
+  console.log(`Starting AnyFlow CLI v${version}...`);
 
   const program = new Command();
 
-  console.log(figlet.textSync("Anyflow CLI"));
+  printHeader();
+
   try {
     program
       .name("anyflow")
-      .description("CLI for AnyFlow operations, command init to initialize the project")
+      .description("The CLI for AnyFlow operations. Check https://docs.anyflow.pro/docs/anyflow_cli/ to learn more.")
       .version(version);
 
     program
@@ -58,7 +69,7 @@ async function main() {
       .command("logout")
       .description("Clear user credencials")
       .action(logout)
-    
+
     program
       .command("fix")
       .description("Fix failed deployments")
